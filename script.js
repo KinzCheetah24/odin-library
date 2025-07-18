37eee7d1c5ea84ddc4067898649de3aa6a4091a4
Álvaro Pintado <alvaro.pintado@alumnos.upm.es>
2025-07-18 14:20:44 +0200

    The code for the functioning of adding and removing books was added.
    In the html file the div section for displaying the books was added, and in the js file, the code for displaying the book that was added in the document(html) and being able to remove it was written.

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 const myLibrary = [];
 
+const dialog = document.querySelector("dialog");
+const showBtn = document.getElementById("showDialog");
+const library = document.querySelector(".library");
+const bookForm = document.querySelector(".bookForm");
+
 function Book(id, title, author, pages, read) {
     if (!new.target) {
         throw Error("You must use the 'new' operator to call the constructor");
@@ -20,8 +25,14 @@ function Book(id, title, author, pages, read) {
     }
 }
 
+Book.prototype.toogleRead = () => {
+    this.read = !this.read;
+}
+
 function addBookToLibrary(title, author, pages, read) {
-    myLibrary.push(new Book(crypto.randomUUID() ,title, author, pages, read))
+    const book = new Book(crypto.randomUUID() ,title, author, pages, read);
+    myLibrary.push(book);
+    displayBook(book);
 }
 
 function showLibrary() {
@@ -29,6 +40,69 @@ function showLibrary() {
         console.log(myLibrary[i].info());
     }
 }
+
+function displayBook(book) {
+    const bookDiv = document.createElement("div");
+    bookDiv.classList.add("book");
+
+    const removeBtn = document.createElement("button");
+    removeBtn.textContent = "X";
+
+    const bookTitle = document.createElement("h2");
+    bookTitle.textContent = "Title: " + book.title;
+
+    const bookAuthor = document.createElement("h3");
+    bookAuthor.textContent = "Author: " + book.author;
+
+    const bookPages = document.createElement("p");
+    bookPages.textContent = "Pages: " + book.pages;
+
+    const bookRead = document.createElement("p");
+    bookRead.textContent = "Read";
+
+    const bookReadCheckBox = document.createElement("input");
+    bookReadCheckBox.type = "checkbox";
+    bookReadCheckBox.checked = book.read;
+
+    bookDiv.appendChild(removeBtn);
+    bookDiv.appendChild(bookTitle);
+    bookDiv.appendChild(bookAuthor);
+    bookDiv.appendChild(bookPages);
+    bookDiv.appendChild(bookRead);
+    bookDiv.appendChild(bookReadCheckBox);
+    bookDiv.appendChild(bookTitle);
+
+    library.appendChild(bookDiv);
+
+    removeBtn.addEventListener("click", () => removeBook(book.id, bookDiv));
+}
+
+function removeBook(bookId, bookDiv) {
+    const index = myLibrary.findIndex(book => book.id === bookId);
+    if(index !== -1) {
+        myLibrary.splice(index, 1);
+    }
+
+    library.removeChild(bookDiv);
+}
+
+showBtn.addEventListener("click", () => { dialog.showModal(); });
+
+bookForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const title = document.getElementById("title").value;
+    const author = document.getElementById("author").value.trim();
+    const pages = document.getElementById("pages").value;
+    const read = document.getElementById("read").checked;
+
+    addBookToLibrary(title, author, pages, read);
+
+    showLibrary();
+
+    dialog.close();
+    bookForm.reset();
+})
 
 addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 295, false);
 addBookToLibrary("Shadow Hunters", "Cassandra Clare", 500, true);
